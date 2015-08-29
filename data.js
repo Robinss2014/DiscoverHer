366f5cb637f58c561f46dd9f816f17646e484b30
Robinss2014 <robinsswei@gmail.com>
2015-08-29 07:37:43 -0700

    modified data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 $(function(){
     $.getJSON('data.json', function(data) {
-        var events = $('#template').html();
+        var template = $('#template').html();
         var html = Mustache.to_html(template, data);
         $('#target').html(html);
     });
